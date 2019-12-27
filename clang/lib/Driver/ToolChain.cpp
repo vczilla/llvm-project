0c84b523116573ad2c14037603fc8424b15cb7f2
vczilla <vczilla@gmail.com>
2019-12-27 05:20:47 +0000

    Wip

--- clang/lib/Driver/ToolChain.cpp
+++ clang/lib/Driver/ToolChain.cpp
@@ -894,7 +894,7 @@ void ToolChain::AddCXXStdlibLibArgs(const ArgList &Args,
 
   switch (Type) {
   case ToolChain::CST_Libcxx:
-    CmdArgs.push_back("-lc++_shared");
+    CmdArgs.push_back("-lc++");
     break;
 
   case ToolChain::CST_Libstdcxx:
